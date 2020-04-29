cf4ab07d3bf41bdda46d740cad8b73dedc507bc1
emilie <descours.emilie@hotmail.fr>
2020-04-29 16:20:10 +0200

    ajout securiter des entrée

--- src/main/Donjons_et_Dragons.java
+++ src/main/Donjons_et_Dragons.java
@@ -6,18 +6,32 @@ public class Donjons_et_Dragons {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int vie;
+		int force;
 		while (true) {
 
 			Scanner sc = new Scanner(System.in);
 			System.out.println("Quel personnage souhaitez-vous créer ?: (G)uerrier ou (M)agicien ");
 			String str = sc.nextLine();
-			System.out.println("Quel nom voulez-vous donnerà votre personnage ?");
+			System.out.println("Quel nom voulez-vous donner à votre personnage ?");
 			String nom = sc.nextLine();
-			System.out.println("Combien de vie voulez-vous donnerà votre personnage ?");
-			int vie = sc.nextInt();
-			System.out.println("Combien de force voulez-vous donnerà votre personnage ?");
-			int force = sc.nextInt();
+			System.out.println("Combien de vie voulez-vous donner à votre personnage ?");
+			try {
+				vie = sc.nextInt();
+			}catch(Exception InputMismatchException){
+				System.out.println("Paramètre invalide");
+				vie = 0;
+			}
+			System.out.println("Combien de force voulez-vous donner à votre personnage ?");
+			try {
+				force = sc.nextInt();
+			}catch(Exception InputMismatchException){
+				System.out.println("Paramètre invalide");
+				force = 0;
+			}
+			
 			System.out.println("Vous avez saisi : " + str);
+			
 			if (str.equals("G")) {
 				if (!nom.equals("") && vie != 0 && force != 0) {
 					try {
@@ -28,8 +42,8 @@ public class Donjons_et_Dragons {
 						vie = 0;
 						force = 0;
 					}
-
 				}
+				
 				if (!nom.equals("") && vie == 0 && force == 0) {
 					Guerrier jo = new Guerrier(nom);
 					System.out.println(jo.nom);
@@ -38,10 +52,18 @@ public class Donjons_et_Dragons {
 					System.out.println(jo.nom);
 				}
 			} else if (str.equals("M")) {
+				
 				if (!nom.equals("") && vie != 0 && force != 0) {
-					Magicien lulu = new Magicien(nom, vie, force);
-					System.out.println(lulu.nom + " " + lulu.niveau_de_vie + " " + lulu.force_attaque);
-				}
+					try {
+						Magicien lulu = new Magicien(nom, vie, force);
+						System.out.println(lulu.nom + " " + lulu.niveau_de_vie + " " + lulu.force_attaque);
+					}catch (Exception IllegalArgumentExeception ) {
+						System.out.println("Paramètre invalide");
+						vie = 0;
+						force = 0;
+					}
+					}
+					
 
 				else if (!nom.equals("")) {
 
--- src/main/Guerrier.java
+++ src/main/Guerrier.java
@@ -12,25 +12,28 @@ public class Guerrier {
 	int max_force_attaque = 10;
 	int min_force_attaque = 5;
 
-	public Guerrier () {
+	public Guerrier() {
 		this.nom = "undefined";
 		this.url = "undefined";
 		this.niveau_de_vie = 5;
 		this.force_attaque = 8;
-		this.arme= new Arme();
-		this.bouclier =new Bouclier();
+		this.arme = new Arme();
+		this.bouclier = new Bouclier();
 	}
-	public Guerrier (String nouveau_nom) {
+
+	public Guerrier(String nouveau_nom) {
 		this.nom = nouveau_nom;
 		this.url = "undefined";
 		this.niveau_de_vie = 5;
 		this.force_attaque = 8;
-		this.arme= new Arme();
-		this.bouclier =new Bouclier();
+		this.arme = new Arme();
+		this.bouclier = new Bouclier();
 	}
-	
-	public Guerrier (String nouveau_nom, int nouvelle_vie, int nouvelle_force_attaque) {
-		if(nouvelle_vie > max_niveau_de_vie || nouvelle_vie < min_niveau_de_vie || nouvelle_force_attaque > max_force_attaque || nouvelle_force_attaque < min_force_attaque) {
+
+	public Guerrier(String nouveau_nom, int nouvelle_vie, int nouvelle_force_attaque) {
+
+		if (nouvelle_vie > max_niveau_de_vie || nouvelle_vie < min_niveau_de_vie
+				|| nouvelle_force_attaque > max_force_attaque || nouvelle_force_attaque < min_force_attaque) {
 			// le videur nous dit non tu ne rentres pas
 			throw new IllegalArgumentException("Paramètre invalide");
 		}
@@ -38,8 +41,8 @@ public class Guerrier {
 		this.url = "undefined";
 		this.niveau_de_vie = nouvelle_vie;
 		this.force_attaque = nouvelle_force_attaque;
-		this.arme= new Arme();
-		this.bouclier =new Bouclier();
+		this.arme = new Arme();
+		this.bouclier = new Bouclier();
 	}
-	
+
 }
